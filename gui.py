6ac34426a199e70804068bef7c88fea304af5407
momikimasashi <mngjxd03170317@gmail.com>
2018-12-28 20:50:13 +0900

    セーブ、ロードボタンの機能を追加

--- gui.py
+++ gui.py
@@ -1,25 +1,50 @@
 # coding: UTF-8
 
 import os
+import pickle
 import tkinter as tk
 from tkinter import ttk
 import tkinter.filedialog
 
+setting = {"Run Sim":"", "Make Pattern":"", "Make Param":""}
+
 def clicked_run_button():
+    print("Run Start")
     print(v1.get())
     print(entry.get())
+    print("Run End")
+
+def clicked_save_button():
+    print("Save setting file")
+    fTyp = [("", "*.pkl")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    save_file = tkinter.filedialog.asksaveasfilename(filetypes=fTyp, initialdir=iDir)
+    with open(save_file, mode="wb") as pf:
+        pickle.dump(setting, pf)
+    print(setting)
+
+def clicked_load_button():
+    print("Load setting file")
+    fTyp = [("", "*.pkl")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    load_file = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
+    with open(load_file, mode="rb") as pf:
+        setting = pickle.load(pf)
+    print(setting)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, setting[v1.get()])
 
 def clicked_sel_button(entry):
-    fTyp = [("", "*")]
+    fTyp = [("", "*.txt")]
     iDir = os.path.abspath(os.path.dirname(__file__))
     if (entry.get() != ""):
         iDir = entry.get()
     pull_down_elem = v1.get()
-    file_or_dir = ""
-    if (pull_down_elem == "Run Sim"): file_or_dir = tkinter.filedialog.askdirectory(initialdir=iDir)
-    else:                             file_or_dir = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
+    setting[pull_down_elem] = ""
+    if (pull_down_elem == "Run Sim"): setting[pull_down_elem] = tkinter.filedialog.askdirectory(initialdir=iDir)
+    else:                             setting[pull_down_elem] = tkinter.filedialog.askopenfilename(filetypes=fTyp, initialdir=iDir)
     entry.delete(0, tk.END)
-    entry.insert(tk.END, file_or_dir)
+    entry.insert(tk.END, setting[pull_down_elem])
 
 def select_elem(event):
     # ファイル選択、フォルダ選択
@@ -33,6 +58,7 @@ def select_elem(event):
         fileSelectButton.state(["disabled"])
         label_value.set("you push run bottun only")
     entry.delete(0, tk.END)
+    entry.insert(tk.END, setting[v1.get()])
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -55,11 +81,12 @@ if __name__ == "__main__":
     runButton = ttk.Button(frame1, text="Run", command=clicked_run_button)
     runButton.grid(row=0, column=1)
 
-    # save,load button
-    saveButton = ttk.Button(frame1, text="save", command=clicked_run_button)
+    # save button
+    saveButton = ttk.Button(frame1, text="save", command=clicked_save_button)
     saveButton.grid(row=2, column=2)
 
-    loadButton = ttk.Button(frame1, text="load", command=clicked_run_button)
+    # load button
+    loadButton = ttk.Button(frame1, text="load", command=clicked_load_button)
     loadButton.grid(row=2, column=3)
 
     # Label
@@ -67,7 +94,6 @@ if __name__ == "__main__":
     label_value.set("select output dir")
     Label = tk.Label(frame1, textvariable=label_value)
     Label.grid(row=1, column=0)
-
 
     # path入力と選択ボタン
     entry = tk.Entry(frame1)
